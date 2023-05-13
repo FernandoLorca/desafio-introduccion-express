14ac22a5bfe40251a8a27be345728b8395d12fab
FernandoLorca <florcapinto@gmail.com>
2023-05-12 20:58:23 -0400

    change the read and write functions from synchronous to  asynchcronous. And create the readme file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import fs from 'fs';
+import fs from 'fs/promises';
 import { nanoid } from 'nanoid';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -13,89 +13,157 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.get('/songs', (req, res) => {
-  const songs = JSON.parse(fs.readFileSync('repertory.json'));
-  res.json(songs);
-});
-
-app.post('/songs', (req, res) => {
-  const { song, artist, tune } = req.body;
-  const songs = JSON.parse(fs.readFileSync('repertory.json'));
-
-  const newSong = {
-    id: nanoid(),
-    song,
-    artist,
-    tune,
-  };
-
-  if (song !== '' || artist !== '' || tune !== '') {
-    songs.push(newSong);
-  } else {
-    return res.status(401).send('Song, artist or tune are not valid!');
-  }
-
-  fs.writeFileSync('repertory.json', JSON.stringify(songs));
-  res.status(201).json({
-    ok: true,
-    msg: 'Song added successfully',
-    songBody: req.body,
-  });
-});
-
-app.delete('/songs/:id', (req, res) => {
-  const { id } = req.params;
-  const songs = JSON.parse(fs.readFileSync('repertory.json'));
-  const songIndex = songs.findIndex(song => song.id === id);
-  const songDeleted = songs.filter(song => song.id === id);
-  songs.splice(songIndex, 1);
-  fs.writeFileSync('repertory.json', JSON.stringify(songs));
-  res.status(200).json({
-    ok: true,
-    msg: 'Song deleted successfully',
-    songDeleted: songDeleted,
-  });
-});
-
-app.put('/songs/:id', (req, res) => {
-  const { id } = req.params;
-  const { song, artist, tune } = req.body;
-  const songs = JSON.parse(fs.readFileSync('repertory.json'));
-  const indexSong = songs.findIndex(song => song.id === id);
-  const songUpdated = songs.filter(song => song.id === id);
-
-  if (indexSong === -1)
-    return res.json({
+app.get('/songs', async (req, res) => {
+  // const songs = JSON.parse(fs.readFileSync('repertory.json'));
+  try {
+    const data = await fs.readFile('repertory.json', 'utf8');
+    const songs = JSON.parse(data);
+    res.json(songs);
+  } catch (error) {
+    res.status(500).json({
       ok: false,
-      error: 404,
-      msg: 'Song not found',
+      error: error,
+      msg: 'There was an error reading the file',
     });
+  }
+});
 
-  if (song !== '' && artist !== '' && tune !== '') {
-    const { id } = songs[indexSong];
+app.post('/songs', async (req, res) => {
+  const { song, artist, tune } = req.body;
+  // const songs = JSON.parse(fs.readFileSync('repertory.json'));
+  try {
+    const data = await fs.readFile('repertory.json', 'utf8');
+    const songs = await JSON.parse(data);
 
-    const songUpdate = {
-      id,
+    const newSong = {
+      id: nanoid(),
       song,
       artist,
       tune,
     };
-    songs[indexSong] = songUpdate;
 
-    fs.writeFileSync('repertory.json', JSON.stringify(songs));
-    res.json({
+    if (song !== '' || artist !== '' || tune !== '') {
+      songs.push(newSong);
+    } else {
+      return res.status(401).send('Song, artist or tune are not valid!');
+    }
+
+    try {
+      await fs.writeFile('repertory.json', JSON.stringify(songs));
+    } catch (writeError) {
+      res.status(500).json({
+        ok: false,
+        error: writeError,
+        msg: 'There was an error writing to the file',
+      });
+    }
+
+    res.status(201).json({
       ok: true,
-      msg: 'Song updated successfully',
-      dataChange: {
-        oldSong: songUpdated[0],
-        newSong: songUpdate,
-      },
+      msg: 'Song added successfully',
+      songBody: req.body,
     });
-  } else {
+  } catch (readError) {
     res.json({
       ok: false,
-      error: 400,
-      msg: 'Data must be something',
+      error: readError,
+      msg: 'There was an error adding the song',
+    });
+  }
+});
+
+app.delete('/songs/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const data = await fs.readFile('repertory.json');
+    const songs = JSON.parse(data);
+    const songIndex = songs.findIndex(song => song.id === id);
+    const songDeleted = songs.filter(song => song.id === id);
+    songs.splice(songIndex, 1);
+    res.status(200).json({
+      ok: true,
+      msg: 'Song deleted successfully',
+      songDeleted: songDeleted,
+    });
+
+    try {
+      await fs.writeFile('repertory.json', JSON.stringify(songs));
+    } catch (writeError) {
+      res.status(500).json({
+        ok: false,
+        error: writeError,
+        msg: 'There was an error writing the file',
+      });
+    }
+  } catch (readError) {
+    res.status(500).json({
+      ok: false,
+      error: readError,
+      msg: 'There was an error deliting the song',
+    });
+  }
+});
+
+app.put('/songs/:id', async (req, res) => {
+  const { id } = req.params;
+  const { song, artist, tune } = req.body;
+
+  try {
+    const data = await fs.readFile('repertory.json');
+    const songs = JSON.parse(data);
+    const indexSong = songs.findIndex(song => song.id === id);
+
+    if (indexSong === -1)
+      return res.json({
+        ok: false,
+        error: 404,
+        msg: 'Song not found',
+      });
+
+    const songUpdated = songs.filter(song => song.id === id);
+
+    if (song !== '' && artist !== '' && tune !== '') {
+      const { id } = songs[indexSong];
+
+      const songUpdate = {
+        id,
+        song,
+        artist,
+        tune,
+      };
+      songs[indexSong] = songUpdate;
+
+      try {
+        await fs.writeFile('repertory.json', JSON.stringify(songs));
+      } catch (writeError) {
+        res.status(500).json({
+          ok: false,
+          error: writeError,
+          msg: 'There was an error writing the file.',
+        });
+      }
+
+      res.json({
+        ok: true,
+        msg: 'Song updated successfully',
+        dataChange: {
+          oldSong: songUpdated[0],
+          newSong: songUpdate,
+        },
+      });
+    } else {
+      res.json({
+        ok: false,
+        error: 400,
+        msg: 'Data must be something',
+      });
+    }
+  } catch (readError) {
+    res.status(500).json({
+      ok: false,
+      error: readError,
+      msg: 'There was an error reading the file',
     });
   }
 });
