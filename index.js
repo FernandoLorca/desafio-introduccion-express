eb23c0786efff6f6775295c4c15e15e22b16723f
FernandoLorca <florcapinto@gmail.com>
2023-05-15 20:00:46 -0400

    doing some changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 app.get('/songs', async (req, res) => {
   // const songs = JSON.parse(fs.readFileSync('repertory.json'));
